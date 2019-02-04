0b271f1f1076282601d2c35873a96af130872bac
Jamez Stokes <jamezstokes@MacBook-Pro-7.local>
2019-02-04 14:58:44 -0500

    added attendee class with getter and setter.

---
+++ src/com/company/Attendee.java
@@ -0,0 +1,68 @@
+package com.company;
+
+public class Attendee {
+
+    private String name;
+    private String address;
+    private String arrivalDate;
+    private String specialRequest;
+    private String idNumber;
+    private boolean VIP;
+    private String state;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getArrivalDate() {
+        return arrivalDate;
+    }
+
+    public void setArrivalDate(String arrivalDate) {
+        this.arrivalDate = arrivalDate;
+    }
+
+    public String getSpecialRequest() {
+        return specialRequest;
+    }
+
+    public void setSpecialRequest(String specialRequest) {
+        this.specialRequest = specialRequest;
+    }
+
+    public String getIdNumber() {
+        return idNumber;
+    }
+
+    public void setIdNumber(String idNumber) {
+        this.idNumber = idNumber;
+    }
+
+    public boolean isVIP() {
+        return VIP;
+    }
+
+    public void setVIP(boolean VIP) {
+        this.VIP = VIP;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+}
