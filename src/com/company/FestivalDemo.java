3eb421ed7a695d91c999dc1908b90ba6cd725357
Jamez Stokes <jamezstokes@MacBook-Pro-7.local>
2019-02-04 15:12:33 -0500

    adding user menu method to demo.

--- src/com/company/FestivalDemo.java
+++ src/com/company/FestivalDemo.java
@@ -4,5 +4,10 @@ public class FestivalDemo {
 
     public static void main(String[] args) {
 	// write your code here
+
+        UserMenu menu = new UserMenu();
+
+        menu.showMainMenu();
+
     }
 }
---
+++ src/com/company/UserMenu.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class UserMenu {
+
+    public void showMainMenu () {
+        System.out.println("Would you like to \n" +
+                "1. Register for the Fyre Festival \n" +
+                "2. Look up a account");
+    }
+
+}
